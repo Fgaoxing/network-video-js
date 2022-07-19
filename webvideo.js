a2452de78df4c30f10d938725b0ae97e1be3cc11
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-07-19 23:11:50 +0800

    Update webvideo.js

--- webvideo.js
+++ webvideo.js
@@ -1,14 +1,53 @@
-function webvideo_view() {
-    var videos = document.getElementsByTagName('webvideo');
+function vqq() {
+    var vqqs = document.getElementsByTagName('vqq');
     for (var i = 0; i < videos.length; i++) {
-        var type = videos[i].getAttribute('type');
-        var ID = videos[i].getAttribute('ID');
-        videosJson={
-            bilibili: '<iframe height=720 width=1280 src="https://player.bilibili.com/player.html?aid=301012336&bvid='+ID+'&cid=777039405&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>',
-            tenc: '<iframe height=720 width=1280 frameborder="0" src="https://v.qq.com/txp/iframe/player.html?vid='+ID+'" allowFullScreen="true"></iframe>',
-            youku: "<iframe height=720 width=1280 src='https://player.youku.com/embed/"+ID+" frameborder=0 'allowfullscreen'></iframe>"
+        var type = vqqs[i].getAttribute('type');
+        var ID = vqqs[i].getAttribute('ID');
+        var width = vqqs[i].getAttribute('width');
+        var height = vqqs[i].getAttribute('height');
+        var vqqJson={
+            player: '<iframe height=720 width=1280 frameborder="0" src="https://v.qq.com/txp/iframe/player.html?vid='+ID+'" allowFullScreen="true" width="`+width+`" height="`+height+`"></iframe>',
         }
-        videos[i].innerHTML = videosJson[type];
+        videos[i].innerHTML = vqqJson[type];
+    }
+}
+
+function bili() {
+    //遍历所有的bili标签
+    var bilis = document.getElementsByTagName('bili');
+    //遍历所有的bili标签
+    for (var i = 0; i < bilis.length; i++) {
+        var type = bilis[i].getAttribute('type');
+        var width = bilis[i].getAttribute('width');
+        var height = bilis[i].getAttribute('height');
+        var BiliJson = {
+            player: {
+                className: 'bili-player', js: function () {
+                    if (bilis[i].getAttribute('bv')) {
+                        bilis[i].innerHTML = `<iframe src="//player.bilibili.com/player.html?bvid=` + bilis[i].getAttribute('bv') + `" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" high_quality="1" danmaku="1" width="`+width+`" height="`+height+`"></iframe>`
+                    } else if (bilis[i].getAttribute('aid')) {
+                        bilis[i].innerHTML = `<iframe src="//player.bilibili.com/player.html?aid=` + bilis[i].getAttribute('aid') + `" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" high_quality="1" danmaku="1" width="`+width+`" height="`+height+`"> </iframe>`
+                    } else if (bilis[i].getAttribute('cid')) {
+                        bilis[i].innerHTML = `<iframe src="//player.bilibili.com/player.html?cid=` + bilis[i].getAttribute('cid') + `" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" high_quality="1" danmaku="1" width="`+width+`" height="`+height+`"> </iframe>`
+                    } else if (bilis[i].getAttribute('url')) {
+                            if (bilis[i].getAttribute('url').indexOf('BV')<=0) {
+                                bilis[i].setAttribute('bv','BV'+bilis[i].getAttribute('url').split('?')[0].split('BV')[1]);
+                                BiliJson[type].js()
+                            } else {
+                                bilis[i].setAttribute('av','av'+bilis[i].getAttribute('url').split('?')[0].split('av')[1]);
+                                BiliJson[type].js()
+                            }
+                    } else {
+                        print_err('你需要一个参数，av、bv或cid', bilis[i]);
+                    }
+                }
+            }
+        }
+        if (BiliJson[type].js){
+            BiliJson[type].js()
+        } else {
+            bilis[i].innerHTML = BiliJson[type].html;
+        }
     }
 }
 
@@ -18,12 +57,12 @@ function webvideo_view() {
 
 if (window.onload) {
     window.onload += function () {
-        webvideo_view()
+        vqq();
+        bili();
     }
-}
-
-else {
+} else {
     window.onload = function() {
-        webvideo_view()
+       vqq();
+        bili();
     }
 }
